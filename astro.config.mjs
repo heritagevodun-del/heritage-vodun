1ce7391df327f229ff788df6afe45732ff3202ed
Heritage Vodun <heritagevodun@gmail.com>
2025-12-15 03:39:38 +0100

    Forcer mode static pour sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
-import vercel from '@astrojs/vercel/static';
+import vercel from '@astrojs/vercel/static'; // On utilise l'import static spécifique
 
 export default defineConfig({
+  // C'EST LA LIGNE MANQUANTE QUI VA TOUT CHANGER :
+  output: 'static',
+
   site: 'https://heritage-vodun.vercel.app',
   
   integrations: [sitemap()],
