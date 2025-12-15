d6d835ce68dad57b708de7e275d7fe04717edb42
Heritage Vodun <heritagevodun@gmail.com>
2025-12-15 03:34:20 +0100

    Forcer la config vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
-import vercel from '@astrojs/vercel'; // On retire le "/static" ici
+import vercel from '@astrojs/vercel/static';
 
-// https://astro.build/config
 export default defineConfig({
   site: 'https://heritage-vodun.vercel.app',
   
-  // On précise qu'on veut un site statique (rapide)
-  output: 'static',
-
   integrations: [sitemap()],
   
-  // On ajoute une option factice "webAnalytics" pour satisfaire VS Code
-  adapter: vercel({
-    webAnalytics: { enabled: true } 
-  })
+  // @ts-ignore
+  adapter: vercel()
 });
