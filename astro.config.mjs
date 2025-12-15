dc319cfc9dce6a996ad3d24604644d463778e293
Heritage Vodun <heritagevodun@gmail.com>
2025-12-15 03:15:50 +0100

    Activation SEO : Sitemap et Meta Tags

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
-import vercel from '@astrojs/vercel';
+import sitemap from '@astrojs/sitemap';
+import vercel from '@astrojs/vercel/static'; // Assurez-vous d'avoir "/static" ici
 
 // https://astro.build/config
 export default defineConfig({
-  adapter: vercel()
+  site: 'https://heritage-vodun.vercel.app',
+  
+  integrations: [sitemap()],
+  
+  // CORRECTION ICI : On ajoute des accolades {} vides
+  adapter: vercel({}) 
 });
