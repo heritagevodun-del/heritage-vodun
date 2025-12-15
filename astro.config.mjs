3c49854a27021b3a826f874606e2d76b86f6820f
Heritage Vodun <heritagevodun@gmail.com>
2025-12-15 03:29:24 +0100

    Fix config vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,19 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
-import vercel from '@astrojs/vercel/static';
+import vercel from '@astrojs/vercel'; // On retire le "/static" ici
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://heritage-vodun.vercel.app',
   
+  // On précise qu'on veut un site statique (rapide)
+  output: 'static',
+
   integrations: [sitemap()],
   
-  // LA SOLUTION : On met des accolades vides ici v
-  adapter: vercel({}) 
+  // On ajoute une option factice "webAnalytics" pour satisfaire VS Code
+  adapter: vercel({
+    webAnalytics: { enabled: true } 
+  })
 });
